d29a4e480586f8909fbe43f1c952c70fd91710a8
rhane <emmarhanenapier@gmail.com>
2015-04-15 21:56:25 -0400

    hip day moved thing to make the library work properly

--- Scotty/Scotty.ino
+++ Scotty/Scotty.ino
@@ -18,46 +18,48 @@ void loop() {
   String frontText = "Front:";
   String fString = frontText + frontDis;
   Serial.println(fString);
-  
-   
 
-  if (frontDis > minDis && leftDis > minDis && rightDis > minDis) 
-  {
-      forward();
-      Serial. println("forwrd");
-  }  
-  else if (leftDis < 10)
-  {
-    right();
-  }
-  else if (rightDis < 10)
-  {
-    left();
-  }
-  else if ( frontDis < minDis)
-  {
-      if (leftDis < minDis)
-      {  
-        right();
-        Serial. println("right");
-      } 
-      else if ( frontDis < 5 )
-      {
-        backward();
-        Serial. println("back");
-      }
-      else
-      {
-        left();
-        Serial. println("left");
-      }      
-  }
-  else {
-     robotStop();
-  } 
+forward();
+
+
+
+
+//if (frontDis > minDis && leftDis > minDis && rightDis > minDis) 
+  //{
+    //  forward();
+      //Serial. println("forwrd");
+//  }  
+ // else if (leftDis < 10)
+ // {
+//    right();
+//  }
+//  else if (rightDis < 10)
+//  {
+//    left();
+ // }
+//  else if ( frontDis < minDis)
+ // {
+  //    if (leftDis < minDis)
+    //  {  
+      //  right();
+        //Serial. println("right");
+//      } 
+  //    else if ( frontDis < 5 )
+    //  {
+      //  backward();
+        //Serial. println("back");
+//      }
+  //    else
+    //  {
+      //  left();
+//        Serial. println("left");
+  //    }      
+//  }
+ // else {
+ //    robotStop();
+//  } 
   
   
-     delay(500);
+//     delay(500);
 }
-    
-    
+   
